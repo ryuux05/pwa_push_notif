7c60380c862fa56c1bb8302207ca8f47bae70c6e
ryuux05 <111038563+ryuux05@users.noreply.github.com>
2023-03-29 14:21:39 +0900

    fix server

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ let VAPID_KEY = {
 push.setVapidDetails('mailto:bryan@sushitopmarketing.com', VAPID_KEY.publicKey, VAPID_KEY.privateKey)
 
 //This is what we pull out from our database of stored people who want to receive notification
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cWeGXW7cyms:APA91bHlQUnOtc4i5k2Be2a-mwC-GYzyNo1WUdjTjeAvTWGtQnCvmmXz4r1NggZf5Y5WWsdZRR1MkDlrNYrOdmAdY_ELXCrs-QsdB1bUkAj1WlwLMQZX2z-KP0YWaLysjydvyuRVkJje","expirationTime":null,"keys":{"p256dh":"BDR5-zWWhTr1Fp3a3pMR6BBgyaIRnMKjYnPG05Pe5qI16opFtJuM_6hR8BknZp3ewfrD5c1QB1LkNEnNy8HFLtk","auth":"Qd-Rx5Mcv-3_RRzVpELQMw"}};
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -1,6 +1,23 @@
 self.addEventListener('push', (event) => {
-    self.registration.sendNotification('Test message', {})
-})
+    var options = {
+        body: "Push Notif Test",
+        icon: "images/example.png",
+        vibrate: [100,50,100],
+        data: {
+            dateOfArrival: date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {action: 'explore', title:'Explore this new world',
+                icon: 'images/checkmark.png'},
+            {action: 'close', title:'Close',
+            icon: 'images/xmark.png'},
+    ]
+    };
+    event.waitUntil(
+        self.registration.showNotification('Hello world', options)
+    );
+});
 
 self.addEventListener('notificationclick', async function(event){
     if(!event.action)
