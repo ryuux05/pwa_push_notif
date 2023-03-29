46dcb12ba39d622d26eabf20a39e6c8521a27e16
ryuux05 <111038563+ryuux05@users.noreply.github.com>
2023-03-29 16:03:08 +0900

    changed server

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ let VAPID_KEY = {
 push.setVapidDetails('mailto:bryan@sushitopmarketing.com', VAPID_KEY.publicKey, VAPID_KEY.privateKey)
 
 //This is what we pull out from our database of stored people who want to receive notification
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cWeGXW7cyms:APA91bHlQUnOtc4i5k2Be2a-mwC-GYzyNo1WUdjTjeAvTWGtQnCvmmXz4r1NggZf5Y5WWsdZRR1MkDlrNYrOdmAdY_ELXCrs-QsdB1bUkAj1WlwLMQZX2z-KP0YWaLysjydvyuRVkJje","expirationTime":null,"keys":{"p256dh":"BDR5-zWWhTr1Fp3a3pMR6BBgyaIRnMKjYnPG05Pe5qI16opFtJuM_6hR8BknZp3ewfrD5c1QB1LkNEnNy8HFLtk","auth":"Qd-Rx5Mcv-3_RRzVpELQMw"}};
+let sub = {"endpoint":"https://web.push.apple.com/QO1FMO4Gg1Mpy7L_wNTXkjQiS4JAniW27u59VS_lbAbkWNbg1OlvomBkKaUpX9613nGCB7Cw5YodAZxschMzM6NJgSile0FYdstBK8t9B0H7Nv2YGPmi9mlhjEw9hTMFYb8dk3O-ibhAbu6ua6OPgmnZk1VEBRWaeftW5fIVooI","keys":{"p256dh":"BJWBofWeePE4JAiu5BsetWpKBWXy7E0m-kjfymt9pbnNYDZ7396fHLEeM5RvXSVqtPyQ5cIy0f_pVzT7BaeqwKo","auth":"Ttd2ml3d8rVH9albeQaUSA"}};
 push.sendNotification(sub, 'test message')
