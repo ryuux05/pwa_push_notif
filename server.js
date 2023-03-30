169ccd7ec972f76e9391de93cdb1ec5979c399ff
ryuux05 <111038563+ryuux05@users.noreply.github.com>
2023-03-30 13:18:14 +0900

    Install prompt added

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ let VAPID_KEY = {
 push.setVapidDetails('mailto:bryan@sushitopmarketing.com', VAPID_KEY.publicKey, VAPID_KEY.privateKey)
 
 //This is what we pull out from our database of stored people who want to receive notification
-let sub = {"endpoint":"https://web.push.apple.com/QO1FMO4Gg1Mpy7L_wNTXkjQiS4JAniW27u59VS_lbAbkWNbg1OlvomBkKaUpX9613nGCB7Cw5YodAZxschMzM6NJgSile0FYdstBK8t9B0H7Nv2YGPmi9mlhjEw9hTMFYb8dk3O-ibhAbu6ua6OPgmnZk1VEBRWaeftW5fIVooI","keys":{"p256dh":"BJWBofWeePE4JAiu5BsetWpKBWXy7E0m-kjfymt9pbnNYDZ7396fHLEeM5RvXSVqtPyQ5cIy0f_pVzT7BaeqwKo","auth":"Ttd2ml3d8rVH9albeQaUSA"}};
+let sub = {"endpoint":"https://web.push.apple.com/QFZq8bi2-k-_Vxa2syjk6yKawQHDJ5zcgC1ZVhbyj4r4jBrwHS46QKDw-hTNmev7lEz9hD4FrlGWPuArIs5GmWA3eVPQp92Cuy1viqxWYM8cMia1n4Cf8ngk16CV4AFICu1TmvR9tgsVUgP_STt6_W8WYcAE0aHPy361JU9CVa4","keys":{"p256dh":"BOGqCFSYJ7wFG8eL0vTNz8VXn3qxxWJ6UZlmvhXoxa0LhM0fSoK7bdYmHmiJMeTz3nJQqg_LBXtHcOz-0EQHb88","auth":"pz0TaZ-3SoJImA6hmsadXA"}};
 push.sendNotification(sub, 'test message')
